7da8a737559073d188f94a96585c4aa3c6cd3075
shashi <shashiweb64@gmail.com>
2025-12-10 16:39:05 +0530

    change back

--- src/Contactus.js
+++ src/Contactus.js
@@ -3,9 +3,11 @@ import React,{Fragment} from 'react'
 function Contactus() {
   return (
     <Fragment>
+      <div style={{backgroundColor:'orange',padding:'20px'}}>
         <h1>heading</h1>
         <p>this is </p>
         <h3>sdfhsjdfhjsdfh</h3>
+        </div>
     </Fragment>
     
   )

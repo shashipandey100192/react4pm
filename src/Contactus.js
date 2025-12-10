3bfd8fa39dd4e78fed3c1850c3e98d9d41eac303
shashi <shashiweb64@gmail.com>
2025-12-10 17:21:30 +0530

    src

--- src/Contactus.js
+++ src/Contactus.js
@@ -1,12 +1,22 @@
 import React,{Fragment} from 'react'
+import abx from './myphoto/myimg.jpg';
 
 function Contactus() {
+
+const a=50;
+const myimg = "https://img.lovepik.com/bg/20240208/hd-landscape-wallpapers-hd-landscape-wallpapers-for-mobile-desktop-wallpaper_3181093_wh860.jpg!/fw/860";
+
+
+
+
   return (
     <Fragment>
       <div style={{backgroundColor:'orange',padding:'20px'}}>
-        <h1>heading</h1>
-        <p>this is </p>
-        <h3>sdfhsjdfhjsdfh</h3>
+        <h1 style={{'background-color':'red'}}>heading</h1>
+        <p style={{fontSize:`${a}px`}}>this is </p>
+        <h3 className='first'>sdfhsjdfhjsdfh</h3>
+        <img src={myimg}/>
+        <img src={abx}/>
         </div>
     </Fragment>
     
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Aboutpage,{Abc} from './About';
 import Contactus from './Contactus';
+import "./style.css";
 
 
 

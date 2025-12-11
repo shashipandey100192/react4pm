5752b49c78d406ca6e9abd7dc63166dd06390880
shashi <shashiweb64@gmail.com>
2025-12-11 17:38:21 +0530

    change

---
+++ src/Bootstrappage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import  {Button} from 'react-bootstrap';
+
+import  Button from 'react-bootstrap/Button';
+
+
+function Bootstrappage() {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-12'>
+                <h1 className='bg-danger p-5 '>use of bootstrap page yyyyyyyyyyyyyy</h1>
+                <Button variant="primary">sdfsdfsdfsdf</Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Bootstrappage
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
 import Aboutpage,{Abc} from './About';
 import Contactus from './Contactus';
 import "./style.css";
+import Bootstrappage from './Bootstrappage';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <Bootstrappage></Bootstrappage>
     <Aboutpage/>
     <Abc/>
+   
   <Contactus/>
  
   </React.StrictMode>
